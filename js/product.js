22a3321d6e408a49f1e1ca9a8982a3b035f01342
SOKHEANG.BOTH <SOKHEANG.BOTH@PNC.LAN>
2023-12-15 08:34:26 +0700

    #2updatefiledashborad

---
+++ js/product.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all delete buttons
+    var deleteButtons = document.querySelectorAll('.btnDelete');
+
+    // Add click event listener to each delete button
+    deleteButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Find the closest parent <tr> element and remove it
+            var row = button.closest('tr');
+            row.remove();
+        });
+    });
+});
+
